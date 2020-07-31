305bd0a46fff6d7b1c3f55ab17e3c6a2ff392b5b
Aodan <aodan@aodan.me>
2020-07-31 16:48:29 +0800

    创建与组合视图 / 构建列表与导航

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,39 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Aodan on 2020/7/30.
+//  Copyright © 2020 aodan. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+        .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+    /**
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                //
+                .previewDisplayName(deviceName)
+                //
+        }
+    }
+ */
+}
